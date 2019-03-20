7e327c6c47b4fb664fa5757eb9908906443abee8
njorogepeter <pgakonde@yahoo.com>
2019-03-20 16:45:43 -0700

    user input

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,4 +18,14 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("connected as id " + connection.threadId + "\n");
+  readProducts();
 });
+function readProducts() {
+    console.log("Selecting all products...\n");
+    connection.query("SELECT * FROM products", function(err, res) {
+      if (err) throw err;
+      // Log all results of the SELECT statement
+      console.table(res);
+      connection.end();
+    });
+  }
